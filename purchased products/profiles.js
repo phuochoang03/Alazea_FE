2832d4dd1028edea97ead3c781f7e30b4f57ff3f
khoaphan23 <khoa05670@gmail.com>
2024-04-21 21:59:34 +0700

    sua

--- purchased products/profiles.js
+++ purchased products/profiles.js
@@ -28,9 +28,6 @@ window.addEventListener('scroll', function() {
       time: "17/04/2024",
     },
   ];
-
-
-
   
 function showProducts() {
   var html = "";
@@ -49,53 +46,6 @@ function showProducts() {
   }
   document.getElementById("tbl").innerHTML = html;
 }
-
-
-
-
-function listProducts() {
-  for (let i = 0; i <= 3; i++) {
-    var demo = '<div class="col-3">';
-    demo += '<div class="card" style="width: 18rem; border:  none">';
-    demo += '<img src="' + Product[i].image + '" class="card-img-top" style="height: 170px; width: 270px; border: none;">'; 
-    // Đặt kích thước ảnh là 100% chiều rộng và 400px chiều cao
-    demo += '<div class="card-body">';
-    demo += '<h5 class="card-title">' + Product[i].name + '</h5>';
-    demo += '<p class="card-text price-red">' + Product[i].price + 'VNĐ</p>';   
-    demo += '</div>'; 
-    demo += '</div>';      
-    demo += '</div>';
-    console.log(demo);
-    document.getElementById("sanphammoi").innerHTML += demo;
-  }
-}
-
-function loadProduct() {
-  showProducts();
-  listProducts()
-}
-
-function createProduct() {
-  var n = Product.length;
-  n++;
-  var productName = document.getElementById("productName").value;
-  var productPrice = document.getElementById("productPrice").value;
-  var productQuantity = document.getElementById("productQuantity").value;
-  var productQuantity = document.getElementById("productTotal").value;
-  var productTime = document.getElementById("productTime").value;
-
-  var newProduct = {
-    id: n,
-    name: productName,
-    price: productPrice,
-    quantity: productQuantity,
-    total: productQuantity,
-    time: productTime,
-  };
-  Product.push(newProduct);
-  showProducts();
-}
-
 function updateProfile() {
   var indexToUpdate = parseInt(document.getElementById("editIndex").value);
   var newName = document.getElementById("newName").value;
